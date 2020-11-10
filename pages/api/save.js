c3337ceb8fd7e5a7188fc88a68fb19af5e08c381
kittdh <henrique.luiz.scremin@gmail.com>
2020-11-09 23:59:25 -0300

    Update save.js

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,4 +1,27 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import credentials from '../../credentials.json'
+
+const doc = new GoogleSpreadsheet('1OSjRrsVu3dXhmxncWWeZnZnk66BxjAH5EEbrPV3nl6s')
+
 export default async (req, res) => {
-  console.log(JSON.parse(req.body))
-  res.end(req.body)
+
+  try {
+    await doc.useServiceAccountAuth(credentials)
+    await doc.loadInfo()
+    const sheet = doc.sheetsByIndex[1]
+    const data = JSON.parse(req.body)
+    //Nome Email Whatsapp Cupom Promo
+    await sheet.addRow({
+      Nome: data.Nome,
+      Email: data.Email,
+      Whatsapp: data.Whatsapp,
+      Cupom: 'aaa111',
+      Promo: 'teste'
+    })
+    res.end(req.body)
+  }
+  catch (err) {
+    console.log(err)
+    res.end('error')
+  }
 }
