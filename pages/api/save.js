c0c1fab5dc97c197f2600b8d79496f934889958e
kittdh <henrique.luiz.scremin@gmail.com>
2020-11-11 23:25:45 -0300

    Code gen and date

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,7 +1,14 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
+import moment from 'moment'
+import { sendStatusCode } from 'next/dist/next-server/server/api-utils'
 import credentials from '../../credentials.json'
 
 const doc = new GoogleSpreadsheet('1OSjRrsVu3dXhmxncWWeZnZnk66BxjAH5EEbrPV3nl6s')
+
+const genCupom = () => {
+  const code = parseInt(moment().format('YYMMDDHHmmssSSS')).toString(16).toUpperCase()
+  return code.substr(0, 4) + '-' + code.substr(4, 4) + '-' + code.substr(8, 4)
+}
 
 export default async (req, res) => {
 
@@ -10,13 +17,31 @@ export default async (req, res) => {
     await doc.loadInfo()
     const sheet = doc.sheetsByIndex[1]
     const data = JSON.parse(req.body)
-    //Nome Email Whatsapp Cupom Promo
+
+    const sheetConfig = doc.sheetsByIndex[2]
+    await sheetConfig.loadCells('A2:B2')
+
+    const mostrarPromocaoCell = sheetConfig.getCell(1, 0)
+    const textoCell = sheetConfig.getCell(1, 1)
+
+    let Cupom = ''
+    let Promo = ''
+
+    if (mostrarPromocaoCell.value === 'VERDADEIRO') {
+      // TODO: GERAR CUPOM
+      Cupom = genCupom()
+      Promo = textoCell.value
+    }
+
+    //Nome - Email - Whatsapp - Data - preenchiemnto - NotaCupom - Promo
     await sheet.addRow({
       Nome: data.Nome,
       Email: data.Email,
       Whatsapp: data.Whatsapp,
-      Cupom: 'aaa111',
-      Promo: 'teste'
+      'Data Preenchimento': moment().format('DD - MMM - YYYY, HH:mm:ss'),
+      Nota: 5,
+      Cupom,
+      Promo
     })
     res.end(req.body)
   }
